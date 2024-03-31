d20fe52f3bf7f811764edb8ea3309cac3fad4140
SQCodes <paulsqmagadi@gmail.com>
2024-03-31 18:29:14 +0300

    modified:   4. Navbar/scripts.js

--- 4. Navbar/scripts.js
+++ 4. Navbar/scripts.js
@@ -51,3 +51,44 @@
 // });
 
 
+
+document.addEventListener("DOMContentLoaded", function() {
+    const scrollContainer = document.querySelector(".nav-container");
+    const scrollLeftBtn = document.querySelector(".left");
+    const scrollRightBtn = document.querySelector(".right");
+
+    scrollLeftBtn.style.display = "none"; // Initially hide the left arrow
+
+    scrollContainer.addEventListener("scroll", function() {
+        // If the scroll is at the beginning, hide the left arrow
+        if (scrollContainer.scrollLeft === 0) {
+            scrollLeftBtn.style.display = "none";
+        } else {
+            scrollLeftBtn.style.display = "block";
+        }
+
+        // If the scroll is at the end, hide the right arrow
+        if (scrollContainer.scrollLeft + scrollContainer.clientWidth === scrollContainer.scrollWidth) {
+            scrollRightBtn.style.display = "none";
+        } else {
+            scrollRightBtn.style.display = "block";
+        }
+    });
+
+    scrollLeftBtn.addEventListener("click", function() {
+        scrollContainer.scroll({
+            left: scrollContainer.scrollLeft - 100,
+            behavior: "smooth"
+        });
+    });
+
+    scrollRightBtn.addEventListener("click", function() {
+        scrollContainer.scroll({
+            left: scrollContainer.scrollLeft + 100,
+            behavior: "smooth"
+        });
+    });
+});
+
+
+
