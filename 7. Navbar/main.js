007b9ff178de73bfd3ed22b8bc0d6da63858cec3
SQCodes <paulsqmagadi@gmail.com>
2024-07-19 18:59:15 +0300

    modified:   7. Navbar/index.html 	modified:   7. Navbar/main.css 	new file:   7. Navbar/main.js

---
+++ 7. Navbar/main.js
@@ -0,0 +1,8 @@
+const searchBar = document.querySelector('.search-mobile-container');
+
+const searchButton = document.querySelector('.search-button-mobile');
+
+
+searchButton.addEventListener('click', function(){
+    searchBar.style.display = 'flex';
+})
